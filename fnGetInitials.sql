2af7b577e13dd666e965ab609137b9693efd602d
storigen <ystasyuk@gmail.com>
2017-11-06 15:33:48 -0800

    Create fnGetInitials.sql

---
+++ fnGetInitials.sql
@@ -0,0 +1,19 @@
+USE [DATABASE]
+
+
+CREATE FUNCTION [dbo].[fnGetInitials] ( @str NVARCHAR(4000) )
+RETURNS NVARCHAR(2000)
+AS
+BEGIN
+    DECLARE @retval NVARCHAR(2000);
+
+    SET @str=RTRIM(LTRIM(@str));
+    SET @retval=LEFT(@str,1);
+
+    WHILE CHARINDEX(' ',@str,1)>0 BEGIN
+        SET @str=LTRIM(RIGHT(@str,LEN(@str)-CHARINDEX(' ',@str,1)));
+        SET @retval+=LEFT(@str,1);
+    END
+
+    RETURN @retval;
+END
